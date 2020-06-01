070eff3c0d5e29699f01383bd19e5984f54f73d8
Ahsan Ali <46025324+ahsanali2000@users.noreply.github.com>
2020-06-02 01:05:07 +0500

    IsOwner permission tested

--- Rest/accounts/api/permissions.py
+++ Rest/accounts/api/permissions.py
@@ -16,6 +16,9 @@ class AnonPermissionOnly(permissions.BasePermission):
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    message = 'You must be the owner of the data'
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        return obj.owner == request.user
