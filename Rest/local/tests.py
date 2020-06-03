d86a2a5e64f7cf99ffeb8697fa97fb5bf5322a97
Ahsan Ali <46025324+ahsanali2000@users.noreply.github.com>
2020-06-04 00:11:13 +0500

    Testing done

--- Rest/local/tests.py
+++ Rest/local/tests.py
@@ -3,17 +3,19 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 from .models import Status
+
 User = get_user_model()
+
 
 class StatusTestCase(TestCase):
     def setUp(self):
-        user=User.objects.create(username='ahsan',email='ahsan@gmail.com')
+        user = User.objects.create(username='ahsan', email='ahsan@gmail.com')
         user.set_password('ahsan')
         user.save()
 
     def test_created_status(self):
-        user=User.objects.get(username='ahsan')
-        obj=Status.objects.create(user=user, content='Some new content using test')
-        self.assertEqual(obj.id,1)
-        qs=Status.objects.all()
-        self.assertEqual(qs.count(),1)
+        user = User.objects.get(username='ahsan')
+        obj = Status.objects.create(user=user, content='Some new content using test')
+        self.assertEqual(obj.id, 1)
+        qs = Status.objects.all()
+        self.assertEqual(qs.count(), 1)
