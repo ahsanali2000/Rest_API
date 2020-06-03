46e0d4108f018fbe56532717c4c2d48a3379b496
Ahsan Ali <46025324+ahsanali2000@users.noreply.github.com>
2020-06-03 23:57:01 +0500

    Testing done

--- Rest/accounts/tests.py
+++ Rest/accounts/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        user=User.objects.create(username='ahsan',email='ahsan@gmail.com')
+        user.set_password('ahsan')
+        user.save()
+
+    def test_created_user(self):
+        qs=User.objects.filter(username='ahsan')
+        self.assertEqual(qs.count(),1)
